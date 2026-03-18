a2698462408a846f8656f945627376ccd60c3075
dishit <dishit@crytotimes.io>
2026-03-18 13:16:43 +0530

    reduce to 1 worker to fix duplicate verification

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ set -o errexit
 echo "Running migrations..."
 python manage.py migrate
 echo "Migrations done. Starting server..."
-gunicorn crypto_times.wsgi:application --bind 0.0.0.0:$PORT --workers 3
+gunicorn crypto_times.wsgi:application --bind 0.0.0.0:$PORT --workers 1
